986b76fea3a0c8d622724b0a94687c102caae3e8
ANTONIOTM\anton <antonio1hua@gmail.com>
2025-08-06 17:22:32 +0200

    Initial project setup with Spring Boot 3.5.4

---
+++ src/main/java/com/antoniotmdev/app_interview/AppInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.antoniotmdev.app_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniotmdev/app_interview/AppInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniotmdev.app_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
