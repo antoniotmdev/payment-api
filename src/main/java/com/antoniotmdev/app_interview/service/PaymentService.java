955a81bdfb8dcf9141b43153358587f136344bd0
ANTONIOTM\anton <antonio1hua@gmail.com>
2025-08-08 17:58:13 +0200

    Add API version to application.properties and update controllers to match new URL structure

--- src/main/java/com/antoniotmdev/app_interview/service/PaymentService.java
+++ src/main/java/com/antoniotmdev/app_interview/service/PaymentService.java
@@ -31,7 +31,7 @@ public class PaymentService {
     /**
      * Create a new {@link PaymentEntity} from the provided DTO.
      *
-     * @param request DTO containing payments details.
+     * @param request DTO containing payment details.
      * @return Persisted {@link PaymentEntity} entity.
      */
     public PaymentEntity createPayment(PaymentRequestDto request){
